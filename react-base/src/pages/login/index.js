f19a2e0531c0e6160c0c6ce4647d2e18b2653295
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-06-16 11:09:17 -0300

    feat:  configurando o axios

--- react-base/src/pages/login/index.js
+++ react-base/src/pages/login/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { Title } from './styled';
+import { Title, Paragrafo } from './styled';
 import { Container } from '../../styles/GlobalStyled';
+import axios from '../../services/axios';
 
 export default function Login() {
+  React.useEffect(() => {
+   async function getDate() {
+    const response= await axios.get('/alunos');
+    const { data } = response;
+    }
+    getDate();
+  }, []);
   return (
     <Container>
-      <Title>Login</Title>
+      <Title>Login
+        <small>Hello World!</small>
+      </Title>
+      <Paragrafo>Lorem ipsum dolor sit amet</Paragrafo>
+      <button type='button'>Enviar</button>
     </Container>
   );
 }

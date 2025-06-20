5e18ba35a977e5db6acf21aa2f4343af5a9df8f5
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-06-20 11:15:22 -0300

    Iniciando a configuração do redux Saga

--- react-base/src/store/modules/example/reducer.js
+++ react-base/src/store/modules/example/reducer.js
@@ -1,14 +1,24 @@
+import * as type from '../types';
 const initialState = {
   botaoClicado: false,
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
-    case 'BOTAO_CLICADO': {
+    case type.BOTAO_CLICADO_SUCCESS: {
       const newState = { ...state };
       newState.botaoClicado = !newState.botaoClicado;
       return newState;
     }
+
+    case type.BOTAO_CLICADO_REQUEST: {
+     return state;
+    }
+
+    case type.BOTAO_CLICADO_FAILURE: {
+     return state;
+    }
+
     default: {
       return state;
     }

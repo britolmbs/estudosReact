5047bc29d9f99155a3ee827f52177f581f62ca25
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-07-08 10:21:26 -0300

    Excluindo alunos

--- react-base/src/pages/Alunos/index.js
+++ react-base/src/pages/Alunos/index.js
@@ -3,7 +3,7 @@ import { Container } from '../../styles/GlobalStyled';
 import axios from '../../services/axios';
 import { AlunoContainer, ProfilePicture } from './styled';
 import { get } from 'lodash';
-import { FaEdit, FaUserCircle, FaWindowClose } from 'react-icons/fa';
+import { FaEdit, FaUserCircle, FaWindowClose, FaExclamation } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import Loading from '../../components/Loading';
 
@@ -20,12 +20,33 @@ export default function Alunos() {
     }
     getData();
   }, []);
+
+  const handleDeleteAsk = e => {
+    e.preventDefault();
+    const exclamation = e.currenteTarget.nextSibling;
+    exclamation.setAtribute('display', 'block');
+    e.currenteTarget.remove();
+  };
+
+  const handleDelete = async (e, id, index) => {
+    e.persist();
+    try{
+      await axios.delete(`/alunos/${id}`);
+     const novosAlunos = {...alunos};
+     novosAlunos.splice(index, 1);
+     setAlunos(novosAlunos);
+     
+    }catch(err){
+      const errors = get(err, 'response.data.errors', []);
+      errors.map(error => toast.error(error));
+    }
+  };
   return (
     <Container>
       <Loading isLoading={isLoading} />
       <h1>Alunos</h1>
       <AlunoContainer>
-        {alunos.map((aluno) => (
+        {alunos.map((aluno, index) => (
           <div key={String(aluno.id)}>
             <ProfilePicture>
               {get(aluno, 'Fotos[0].url', false) ? (
@@ -39,9 +60,10 @@ export default function Alunos() {
             <Link to={`/aluno/${aluno.id}/edit`}>
               <FaEdit size={16} />
             </Link>
-            <Link to={`/aluno/${aluno.id}/delete`}>
+            <Link onClick={handleDeleteAsk} to={`/aluno/${aluno.id}/delete`}>
               <FaWindowClose size={16} />
             </Link>
+            <FaExclamation onClick={e => handleDelete(e, aluno.id, index)} size={16} display='none' cursor='pointer' />
           </div>
         ))}
       </AlunoContainer>

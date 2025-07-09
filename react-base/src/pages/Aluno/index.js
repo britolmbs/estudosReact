a6b0aad42f80c00bef50abefb954237c82e1f5b5
Lucas Brito <britolmbs@hotmail.com>
2025-07-09 19:46:16 -0300

    editando aluno

--- react-base/src/pages/Aluno/index.js
+++ react-base/src/pages/Aluno/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Container } from '../../styled/GlobalStyled';
 import {get} from 'lodash';
+import PropTypes from 'prop-types';
 
 export default function Aluno({ match }){
     const id = get(match, 'params.id', 0);
     return (
         <Container>
-            <h1>Aluno</h1>
+            <h1>{id? 'Editar aluno' : 'Novo Aluno' }</h1>
         </Container>
     )
+}
+Aluno.propType = {
+    match: PropTypes.shape({}).isRequired,
 }

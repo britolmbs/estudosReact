4e73a496f55866871dbf5396ea6cc4f19ae2bbb7
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-07-10 13:34:50 -0300

    Criando o form do cadastro do aluno

--- react-base/src/pages/Aluno/index.js
+++ react-base/src/pages/Aluno/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { Container } from '../../styled/GlobalStyled';
 import {get} from 'lodash';
 import PropTypes from 'prop-types';
+import { Form } from './styled';
 
 export default function Aluno({ match }){
     const id = get(match, 'params.id', 0);
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [sobrenome, setSobrenome] = useState('');
+    const [idade, setIdade] = useState('');
+    const [peso, setPeso] = useState('');
+    const [altura, setAltura] = useState ('');
     return (
         <Container>
-            <h1>{id? 'Editar aluno' : 'Novo Aluno' }</h1>
+            <h1>{id? 'Editar aluno' : 'Novo Aluno'}</h1>
+            <Form>
+                
+                 </Form>
         </Container>
     )
 }

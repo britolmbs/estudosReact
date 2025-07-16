d495605b3727fd53732ad462c910a90b4f6afff6
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-07-16 11:11:44 -0300

    criando a logica para componente de fotos

--- react-base/src/pages/Fotos/index.js
+++ react-base/src/pages/Fotos/index.js
@@ -1,20 +1,48 @@
-import React, { useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { get } from 'lodash';
 import { Container } from '../../components/Loading/styled';
 import Loading from '../../components/Loading';
 import { Title, Form } from './styled';
+import axios from '../../services/axios';
+import toast from 'react-toastify';
+import history from '../../services/history';
+import PropTypes from 'prop-types';
 
-export default function Fotos() {
+
+export default function Fotos({ match }) {
+const id = get(match,  'params.id', '');
   const [isLoading, setIsLoading] = useState(false);
+  const [foto, setFoto] = useState('');
+
+  useEffect(()=> {
+    const getData = async () => {
+      try {
+        setIsLoading(true);
+       const {data} =  await axios.get(`/alunos/${id}`);
+       setFoto(get(data, 'Fotos[0].url', ''));
+       setIsLoading(false)
+      } catch {
+        toast.error('Erro ao obter imagem');
+        setIsLoading(false);
+        history.push('/');
+      }
+
+    }
+  }, []);
   return (
     <Container>
       <Loading isLoading={isLoading} />
       <Title>Fotos</Title>
       <Form>
         <label htmlFor="foto">
+          {foto ? <img src={foto} alt="foto" /> : 'Selecione uma foto'}
           <input type="file" id="foto" />
         </label>
       </Form>
     </Container>
   );
 }
+
+Fotos.propTypes = {
+  match: PropTypes.shape({}).isRequired,
+};
--- react-base/src/pages/Fotos/styled.js
+++ react-base/src/pages/Fotos/styled.js
@@ -11,11 +11,18 @@ export const Form = styled.form`
     heigth: 180px;
     display: flex;
     backgroud: #eee;
+    align-items: center;
+    justify-content: center;
     border: 5px dashed ${colors.primaryColor};
     margin: 30px auto;
     cursor: pointer;
     boder-radius: 50%;
+    overflow: hidden;
   }
+    img {
+    width: 180px;
+    heigth: 180px;
+    }
   input {
     display: none;
   }

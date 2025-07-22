9413a73b55587cae53519f08027dc688bae765df
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-07-22 10:53:47 -0300

    feat: Criando as Primeiras rotas com browserRouter, Routes e Route da aula

--- react-router-dom/src/main.tsx
+++ react-router-dom/src/main.tsx
@@ -2,10 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/global.css'
 import { Home } from './components/Home'
-
+import  { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { About } from './components/About'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )

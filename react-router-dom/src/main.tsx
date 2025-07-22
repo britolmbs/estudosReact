4b5cb74435e431841afce20abe9851875548d15d
Lucas Brito <britolmbs@hotmail.com>
2025-07-22 20:40:16 -0300

    Usando Link e A

--- react-router-dom/src/main.tsx
+++ react-router-dom/src/main.tsx
@@ -4,10 +4,12 @@ import './styles/global.css'
 import { Home } from './components/Home'
 import  { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { About } from './components/About'
+import { Menu } from './components/Menu'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+    <Menu />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/about' element={<About />} />

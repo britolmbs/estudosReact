ff6b1c449053a4ca64052967a7d5b9fcd29a7e81
Lucas Brito <britolmbs@hotmail.com>
2025-07-26 11:56:53 -0300

    state e useLocation: Enviando e recebendo estado

--- react-router-dom/src/components/Menu/index.tsx
+++ react-router-dom/src/components/Menu/index.tsx
@@ -5,7 +5,7 @@ export const Menu = () => {
     return (
         <nav className='menu'>
             <ul>
-                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/' state={'This os state from HOME'}>Home</Link></li>
                 <li><a href="/about">About</a></li>
                 <li><Link to='/post'>Post</Link></li>
                 <li><Link to='/post/10'>Posts 10</Link></li>
--- react-router-dom/src/components/Redirect/index.tsx
+++ react-router-dom/src/components/Redirect/index.tsx
@@ -15,7 +15,9 @@ export const Redirect = () => {
         }, 1000);
 
         if(time <=  0) {
-            navigate('/')
+            navigate('/', {
+                state: `This is the state: ${Math.random()}`,
+            });
         }
 
         return () => {
